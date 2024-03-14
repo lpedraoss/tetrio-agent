83ebbac17a581a825de43152a601918945758d57
lpedraoss <alfonso.suarez.dev@gmail.com>
2024-03-13 21:03:32 -0500

    Predice los movimientos suboptimos, esto para legar a una victoria

--- virtual.py
+++ virtual.py
@@ -8,7 +8,9 @@ from heuristic import Heuristic
 class Tetrio:
 
     def __init__(self):
-        self.board = np.zeros((5, 10), dtype=int)
+        self.board = np.zeros((6, 10), dtype=int)
+        self.board_test=None
+        self.isNone = None
         #self.board = board
         self.checkRotation = checkRotation
         self.piece = None
@@ -18,6 +20,7 @@ class Tetrio:
         self.width = None 
         self.rotations = None  
         self.heuristic = Heuristic()
+
 
     def moveInBoard(self, times=0,initial=2,direction = None,piece = None):
         if piece == None:
@@ -54,70 +57,93 @@ class Tetrio:
             print("esto es de right: ", index)
         return index
 
-    def detectCollision(self, row=-1, initial=3, column=0):
-
-        if column <= (self.width - 1) and row > (-len(self.board)+1):
-            print("detect range: ", self.board[row][initial : initial + self.width-1])
+    def detectCollision(self, row=-1, initial=3, column=0,board=None):
+        if board is None:
+            board = self.board
+        if column <= (self.width - 1) and row > (-len(board)+1):
+            print("detect range: ", board[row][initial : initial + self.width-1])
             index = row -1
-            collision = any(self.board[i][initial+column] == 1 for i in range(index, -len(self.board)-1, -1))
+            collision = any(board[i][initial+column] == 1 for i in range(index, -len(board)-1, -1))
             isPiece = self.piece[self.height-1][column] == 0 and self.board[row][column+initial] == 1
-            isBoard = self.piece[self.height-1][column]==1 and self.board[row][column+initial]==0
-            both = self.piece[self.height-1][column] == 0 and self.board[row][column+initial]==0
+            isBoard = self.piece[self.height-1][column]==1 and board[row][column+initial]==0
+            both = self.piece[self.height-1][column] == 0 and board[row][column+initial]==0
             isHole = isPiece or isBoard or both
             
             if not collision:
                 if isHole:
-                    row = self.detectCollision(row=row, initial=initial, column=column + 1)
+                    row = self.detectCollision(row=row, initial=initial, column=column + 1,board=board)
                 else:
-                    row = self.detectCollision(row=row - 1, initial=initial, column=0)
+                    row = self.detectCollision(row=row - 1, initial=initial, column=0,board=board)
             else:
-                row = self.detectCollision(row=row - 1, initial=initial, column=0)
+                row = self.detectCollision(row=row - 1, initial=initial, column=0,board=board)
         return row
 
-    def addPiece(self, row=2, column=5):
+    def addPiece(self, row=2, column=5,board=None):
+        isBoard = board is None
+        if isBoard:
+            board = self.board
         print("altura de pieza: ", self.height)
         print("ancho de pieza", self.width)
         for i in range(self.height):
             for j in range(self.width):
                 if self.piece[i][j] == 1:
-                    self.board[row + i - self.height + 1][column + j] = 1
+                    board[row + i - self.height + 1][column + j] = 1
+        if self.isNone:
+            self.board = board
+        else:
+            self.board_test = board
 
-
-    def checkLines(self):
+    def checkLines(self,board=None):
+        if board is None:
+            board = self.board
         # Revisa si alguna fila está completamente llena de 1 y haz "pop"
-        full_rows = [idx for idx, row in enumerate(self.board) if all(cell == 1 for cell in row)]
+        full_rows = [idx for idx, row in enumerate(board) if all(cell == 1 for cell in row)]
         for full_row in full_rows:
-            self.cleanLine(full_row)
+            self.cleanLine(row = full_row,board = board)
 
-    def cleanLine(self, row):
-        self.board = np.delete(self.board, row, axis=0)
-        new_row = np.zeros((1, self.board.shape[1]), dtype=int)
-        self.board = np.insert(self.board, 0, new_row, axis=0)
+    def cleanLine(self, row,board=None):
+        if board is None:
+            board = self.board
 
-    def increase_height(self, row):
+        board = np.delete(board, row, axis=0)
+        new_row = np.zeros((1, board.shape[1]), dtype=int)
+        board = np.insert(board, 0, new_row, axis=0)
+        if self.isNone:
+            self.board = board
+        else:
+            self.board_test
+    def increase_height(self, row,board=None):
+            if board is None:
+                board = self.board
             if (row - self.height) < -self.current_height:
                 increaseToAdd = min(4, self.MAX_HEIGHT-1 - self.current_height)
-                increase = np.zeros((increaseToAdd,self.board.shape[1]),dtype = int)
-                self.board = np.vstack((increase,self.board))
+                increase = np.zeros((increaseToAdd,board.shape[1]),dtype = int)
+              
+                self.board = np.vstack((increase, board))
+                
+                self.board_test = np.vstack((increase, board))
                 self.current_height += increaseToAdd
-    def predictMove(self,piece):
-        moves = self.generateMoves(piece=piece)
+
         
-    def selectBestMove(self, moves,board):
+    def selectBestMove(self, moves):
         # Evaluar cada movimiento usando la heurística y seleccionar el mejor
         best_move = None
         best_score = float('inf')
-
+        
         for move in moves:
+            board = np.copy(self.board)
+
+            self.board_test = board
             piece, rot, direction, t, move_column = move
             initial = move_column
             final = move_column + len(pieces[piece][rot][0]) - 1
-            score = self.heuristic.calculate_heuristics(board, initial, final)['score']
+            self.pressAdd(piece=piece,times=t,rotation=rot,dir=direction,board=self.board_test)
+            score = self.heuristic.calculate_heuristics(self.board_test, initial, final)['score']
+            self.board_test = None
 
             if score < best_score:
                 best_score = score
                 best_move = move
-
         return best_move
     
     def generateMoves(self,piece,initial=3):
@@ -141,7 +167,11 @@ class Tetrio:
                         move = self.moveInBoard(times=t, direction=direction,initial=initial,piece=pieceToAdd[rot])
                         moves.append((piece, rot, direction, t, move))
         return moves
-    def pressAdd(self, piece,times,rotation,dir):
+    def pressAdd(self, piece,times,rotation,dir,board=None):
+        self.isNone = board is None
+        
+        if self.isNone:
+            board = self.board
         self.rotations = len(pieces[piece]) 
         #rotate = self.rotations-2
         rotate = rotation
@@ -151,12 +181,8 @@ class Tetrio:
         columnInitial = self.checkRotation(piece=piece,rotate=rotate)
         #altura heuristica:  {'height': 2, 'holes': 2, 'score': 0.03353920515574651}
         column = self.moveInBoard(times=times, initial=columnInitial,direction=dir)
-        moves = self.generateMoves(initial=columnInitial,piece=piece)
-        print('moves: ------- ',moves)
-        best_move = self.selectBestMove(moves,board= self.board)  # Nueva función para seleccionar el mejor movimiento
-        print(best_move, 'mejor jugada!!!!!!!!!!!')
-        row = self.detectCollision(initial=column)
-        self.increase_height(row)
+        row = self.detectCollision(initial=column,board = board)
+        self.increase_height(row=row,board = board)
 
         print("se decide entonces apilarla en la fila: {}".format(row))
         print(
@@ -164,32 +190,68 @@ class Tetrio:
                 column, column + self.width - 1
             )
         )
-        self.addPiece(row=row, column=column)
-        heuristic = self.heuristic.calculate_heuristics(board = self.board,initial=column,final=column+self.width-1)
+        self.addPiece(row=row, column=column,board=board)
+        heuristic = self.heuristic.calculate_heuristics(board = board,initial=column,final=column+self.width-1)
         print("altura heuristica: ", heuristic)
-        self.checkLines()
-        
+        self.checkLines(board=board)
+
+    def predictMove(self,piece):
+        moves = self.generateMoves(piece=piece)    
+        best_move = self.selectBestMove(moves=moves)
+        return best_move
 
     def showBoard(self):
+        print('--------------------BOARD----------------------')
         for i in self.board:
             print(i)
+        print('--------------------BOARD_TEST-------------------')
+        if self.board_test is None:
+            print('BOARD TEST IS EMPTY')
+        else:
+            for i in self.board_test:
+                print(i)
+    def startGame(self):
+        for p in range(2):
+            move = self.predictMove(piece='o')
+            piece, rot, direction, t, move_column = move
+            self.pressAdd(piece=piece,rotation=rot,dir=direction,times=t)
+            print('La mejor jugada es!!!!!!!!!!!!!!!', move)
 
+            move = self.predictMove(piece='i')
+            piece, rot, direction, t, move_column = move
+            self.pressAdd(piece=piece,rotation=rot,dir=direction,times=t)
+            print('La mejor jugada es!!!!!!!!!!!!!!!', move)
+            
+
+            move = self.predictMove(piece='t')
+            piece, rot, direction, t, move_column = move
+            self.pressAdd(piece=piece,rotation=rot,dir=direction,times=t)
+            print('La mejor jugada es!!!!!!!!!!!!!!!', move)
+            
+            
+            move = self.predictMove(piece='z')
+            piece, rot, direction, t, move_column = move
+            self.pressAdd(piece=piece,rotation=rot,dir=direction,times=t)
+            print('La mejor jugada es!!!!!!!!!!!!!!!', move)
+
+            move = self.predictMove(piece='s')
+            piece, rot, direction, t, move_column = move
+            self.pressAdd(piece=piece,rotation=rot,dir=direction,times=t)
+            print('La mejor jugada es!!!!!!!!!!!!!!!', move)
+
+            move = self.predictMove(piece='o')
+            piece, rot, direction, t, move_column = move
+            self.pressAdd(piece=piece,rotation=rot,dir=direction,times=t)
+            print('La mejor jugada es!!!!!!!!!!!!!!!', move)
+            
+        self.showBoard()
+        print(len(self.board))
 
 tetrio = Tetrio()
+tetrio.startGame()
 
 
-def startGame():
-
-    #for p in range(1):
-        #tetrio.pressAdd(piece="l",rotation=0,times=1,dir='left')
-        #tetrio.pressAdd(piece="i",rotation=0,times=2,dir='right')
-        #tetrio.pressAdd(piece="l",rotation=1,times=4,dir='left')
-        #tetrio.pressAdd(piece="t",rotation=0,times=2,dir='left')
-        #tetrio.pressAdd(piece="o",rotation=0,times=1,dir='right')
-        #tetrio.pressAdd(piece="s",rotation=0,times=4,dir='right')
-    print(tetrio.generateMoves(piece="o"),'esto es otra vuelta!!!!!!!!!!!!!!!')
-
-startGame()
 
 
-tetrio.showBoard()
+
+
